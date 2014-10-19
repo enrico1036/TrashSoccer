e3ed9eecca00a2ae541044888ae8200d1096d9de
Enrico Gambini <enrico1036@gmail.com>
2014-10-19 04:36:55 +0200

    Desperate try

--- core/src/com/trashgames/trashsoccer/screen/MenuScreen.java
+++ core/src/com/trashgames/trashsoccer/screen/MenuScreen.java
@@ -1,18 +1,30 @@
 package com.trashgames.trashsoccer.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.trashgames.trashsoccer.GameManager;
+import com.trashgames.trashsoccer.ui.UIButton;
+import com.trashgames.trashsoccer.ui.UILabel;
 
 public class MenuScreen extends GameScreen {
 
+<<<<<<< HEAD
 	//Background Image
 	private Texture bg;
 	private Sprite spriteBg;
 	private SpriteBatch sb;
+=======
+	private float x=0;
+	UIButton button;
+	UILabel label;
+>>>>>>> refs/remotes/origin/matcap_branch
 	
 	public MenuScreen(GameManager gm) {
 		super(gm);
@@ -33,10 +45,16 @@ public class MenuScreen extends GameScreen {
 		super.render(delta);
 		//Clear buffer bit
 		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+<<<<<<< HEAD
 		
 		//Draws all the images
 		sb.begin();
 		spriteBg.draw(sb);
+=======
+		sb.begin();
+		button.render(sb);
+		label.render(sb);
+>>>>>>> refs/remotes/origin/matcap_branch
 		sb.end();
 	}
 
@@ -45,7 +63,38 @@ public class MenuScreen extends GameScreen {
 		super.update(delta);
 		camera.update();
 		
+<<<<<<< HEAD
+=======
+		if(button.checkBound(gm.im.mousePos()))
+			button.setPressed(true);
+		else
+			button.setPressed(false);
 		
+		
+	}
+	@Override
+	public void show() {
+		super.show();
+		sb = new SpriteBatch();
+		camera = new OrthographicCamera(30 , 30*(GameManager.WND_HEIGHT/GameManager.WND_WIDTH));
+		camera.setToOrtho(false);
+		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+>>>>>>> refs/remotes/origin/matcap_branch
+		
+<<<<<<< HEAD
+=======
+		BitmapFont font = new BitmapFont();
+		font.setColor(Color.WHITE);
+		
+		Texture t1 = new Texture(Gdx.files.internal("image.jpg"));
+		Texture t2 = new Texture(Gdx.files.internal("paolo-brosio.jpg"));
+		
+		button = new UIButton("CIAO", font, new Rectangle(200, 200, 100, 100), new Sprite(t1), new Sprite(t2));
+		label = new UILabel("TEST LABEL", new Rectangle(0, 0, 100, 100), font);
+		
+		
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1f);
+>>>>>>> refs/remotes/origin/matcap_branch
 	}
 	
 	@Override
---
+++ core/src/com/trashgames/trashsoccer/ui/UIButton.java
@@ -0,0 +1,97 @@
+package com.trashgames.trashsoccer.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class UIButton {
+	private String text;
+	private BitmapFont font;
+	private Sprite[] backImg;	// 0 = normal background, 1 = pressed background
+	private boolean state; 		// false = released, 	  true = pressed
+	public Rectangle bound;
+	
+	public UIButton(String text, BitmapFont font, Rectangle bound, Sprite normal, Sprite pressed){
+		this.font = font;
+		setText(text);
+		this.backImg = new Sprite[2];
+		backImg[0] = normal;
+		backImg[1] = pressed;
+		this.bound = bound;
+	}
+	
+	public void render(SpriteBatch batch){
+		// Convert boolean to int
+		int s = (state) ? 1 : 0;
+		// Checks if sprite exists
+		if(backImg[s] != null){
+			backImg[s].setBounds(bound.x, bound.y, bound.width, bound.height);
+			backImg[s].setSize(bound.width, bound.height);
+			backImg[s].draw(batch);
+		}
+		// Draws text
+		if(!text.isEmpty()){
+			TextBounds tBound = font.getBounds(text);
+			font.draw(batch, text, bound.x + (bound.width - tBound.width) / 2, bound.y + (bound.height - tBound.height) / 2);
+		}
+	}
+	
+	public boolean checkBound(Vector2 point){
+		return bound.contains(point);
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void setBackground(Sprite backImg) {
+		this.backImg[0] = backImg;
+	}
+	
+	public void setPressedBackground(Sprite backImg) {
+		this.backImg[1] = backImg;
+	}
+
+	public boolean isPressed(){
+		return state;
+	}
+
+	public void setPressed(boolean pressed) {
+		this.state = pressed;
+	}
+	
+	public BitmapFont getFont() {
+		return font;
+	}
+
+	public void setFont(BitmapFont font) {
+		this.font = font;
+	}
+
+	public void setPos(float x, float y){
+		bound.x = x;
+		bound.y = y;
+	}
+	
+	public void setSize(float w, float h){
+		bound.width = w;
+		bound.height = h;
+	}
+	
+	public void setBound(Rectangle rect){
+		bound = rect;
+	}
+	
+	public Rectangle getBound(){
+		return bound;
+	}
+	
+}
